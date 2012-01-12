ce7418f93547495df41b28cf62a3cefb06bf12c3
git03 <git03@naver.com>
2012-01-12 10:29:10 +0900

    add git03Lib by git03

---
+++ git03Lib/git03_lib.java
@@ -0,0 +1,11 @@
+package com.git03.sample;
+
+public class Git03{
+	public String getString(){
+		return "huhuhuhuhu~~~";
+	}
+
+	public static void main(String args[]){
+		System.out.println("hello everybody!!!");
+	}
+}
